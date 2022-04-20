e05fd2f529cfef45e5eff6c3fea92db290826bd1
stud.mipt <stud.mipt@gmail.com>
2022-04-20 19:10:35 +0300

    Heron_func in main

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ print("Объем куба равен ", math_func.cube(a))
 a, b = input("Укажите катеты прямоуг. треуг. через пробел ").strip().split()
 print("Гипотенуза ", math_func.hipot(float(a), float(b)))
 
+a, b, c = float(input("Укажите, 3 стороны треугольника "))
+print("Площадь ", math_func.Heron(a, b, c))
+
 
 
 
