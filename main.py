cfffacaba3ad743bdac7c91f1e8eb525e3f9751a
studmipt <stud.mipt@gmail.com>
2022-04-26 16:54:35 +0300

    Correct all errors

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ print("Объем куба равен ", math_func.cube(a))
 a, b = input("Укажите катеты прямоуг. треуг. через пробел ").strip().split()
 print("Гипотенуза ", math_func.hipot(float(a), float(b)))
 
-a, b, c = float(input("Укажите, 3 стороны треугольника "))
-print("Площадь ", math_func.Heron(a, b, c))
+a, b, c = input("Укажите, 3 стороны треугольника ").strip().split()
+print("Площадь ", math_func.Heron(float(a), float(b), float(c)))
 
 
 
